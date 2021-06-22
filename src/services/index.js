6d7109adbdcbf9c3e80eec966dd146e7a0aca76c
23carnies <46233990+23carnies@users.noreply.github.com>
2021-06-22 15:04:04 -0400

    Add second api call

--- src/services/index.js
+++ src/services/index.js
@@ -1,17 +1,29 @@
-const baseURL = 'https://api.themoviedb.org/3';
-const key = `process.env.REACT_APP_MOVIE_DB_API_KEY`
-const Moment = require('moment')
+// const baseURL = "https://api.themoviedb.org/3";
+const key = `${process.env.REACT_APP_MOVIE_DB_API_KEY}`;
+const Moment = require("moment");
 
 export function getMovies() {
-    return fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=7315ec59ea2264da1fa4f4eb8d647853`)
-    .then(res => res.json())
+  return fetch(
+    `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}`
+  )
+    .then((res) => res.json())
     .then((data) => {
-        const allMovies = Object.values(data.results)
-        const sorted = allMovies.sort((a,b) => (
-            new Moment(b.release_date).format('YYYYMMDD') - new Moment(a.release_date).format('YYYYMMDD')
-            ))
-            // console.log('from services', sorted)
-        return sorted;
-    })
+      const allMovies = Object.values(data.results);
+      const sorted = allMovies.sort(
+        (a, b) =>
+          new Moment(b.release_date).format("YYYYMMDD") -
+          new Moment(a.release_date).format("YYYYMMDD")
+      );
+      // console.log('from services', sorted)
+      return sorted;
+    });
 }
 
+export function getOneMovie(id) {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${key}`
+  )
+  .then((res) => res.json())
+  
+
+}
