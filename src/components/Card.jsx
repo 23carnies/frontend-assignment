5dd903286a65db071c9864179028a8f700da0e85
23carnies <46233990+23carnies@users.noreply.github.com>
2021-06-22 15:02:20 -0400

    Adjust card display

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,56 +1,65 @@
-import React from 'react'
-import styled from 'styled-components';
-import { Flex } from './utilities';
+import React from "react";
+import styled from "styled-components";
+import { Flex } from "./utilities";
 
-const Card = ({movie}) => {
-    return (
-        <>
-        <Outside style={{border:'1px solid black'}}>
-        {/* <p>{movie.poster_path}</p> */}
-        <Image src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} />
-        <Vote><VoteAvg>{movie.vote_average}</VoteAvg></Vote>
-        {/* <p>{movie.release_date}</p> */}
-        <p>{movie.title}</p>
+const Card = ({ movie }) => {
+  return (
+    <>
+      <Container>
+        <Image
+          src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+          alt={`${movie.title} cover`}
+        />
+        <Vote>
+          <VoteAvg>{movie.vote_average}</VoteAvg>
+        </Vote>
+        <Title>{movie.title}</Title>
+      </Container>
+    </>
+  );
+};
 
-        </Outside>
-        </>
-    );
-}
- 
 export default Card;
 
-const Outside = styled.div`
-    width: 280px;
-    height: 346px;
-    background-color: #fff;
-    /* border: 1px solid #E1E3E6; */
-    box-shadow: 1px 2px 8px 2px rgba(0, 0, 0, 0.1);
-    margin: 26px;
-    ${Flex({fd:'column'})}
-    position: relative;
-    border-radius: 8px;
+const Container = styled.div`
+  width: 280px;
+  height: 346px;
+  background-color: #fff;
+  box-shadow: 1px 2px 8px 2px rgba(0, 0, 0, 0.1);
+  margin: 26px;
+  ${Flex({ fd: "column" })}
+  position: relative;
+  border-radius: 8px;
+  overflow: wrap;
 `;
 
 const Image = styled.img`
-    border-radius: 7px 7px 0 0;
-    width: 280px;
-    height: 302px;
-
+  border-radius: 7px 7px 0 0;
+  width: 280px;
+  height: 302px;
+  /* margin-top: -8px; */
 `;
 
 const Vote = styled.div`
-    width: 34px;
-    height: 34px;
-    border-radius: 50%;
-    border: 1px solid #000;
-    background-color: #fff;
-    ${Flex({ai:'center', jc:'center'})};
-    position: absolute;
-    top: 14px;
-    left: 14px;
+  width: 34px;
+  height: 34px;
+  border-radius: 50%;
+  border: 1px solid #000;
+  background-color: #fff;
+  ${Flex({ ai: "center", jc: "center" })};
+  position: absolute;
+  top: 14px;
+  left: 14px;
 `;
 
 const VoteAvg = styled.p`
-    font-weight: 600;
+  font-weight: 600;
+`;
 
+const Title = styled.p`
+  text-align: center;
+  line-height: 20px;
+  letter-spacing: -0.01em;
+  padding: 0 20px;
+  /* margin: 8px; */
 `;
