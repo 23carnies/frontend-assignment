82339b7f371e466a5ebfefacfe657274981f44fc
23carnies <46233990+23carnies@users.noreply.github.com>
2021-06-22 15:03:29 -0400

    Add modal
    Modal functionality and second api call

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,50 +1,58 @@
 import React, { useState, useEffect } from "react";
-import Card from './Card';
+import Card from "./Card";
 import * as MovieAPI from "../services";
-// import { data } from "browserslist";
-import styled from 'styled-components';
-import { Flex } from '../components/utilities';
+import DetailModal from "./DetailModal";
+import styled from "styled-components";
+import { Flex } from "../components/utilities";
 
 const MovieList = () => {
+    const [showModal, setShowModal] = useState(false);
+    // const openModal = () => setShowModal(!showModal);
   const [movies, setMovies] = useState([]);
+  const [movie, setMovie] = useState("");
 
   const fetchMovies = async () => {
     return await MovieAPI.getMovies();
   };
 
-  useEffect(() => {
-      fetchMovies()
-      .then((data) => {
-        //   const movieCount = Object.keys(data.results)
-          const allMovies = Object.values(data.results)
-          console.log(allMovies.release_date)
-        //   console.log('list',allMovies)
-          setMovies(allMovies)
-      })
-  },[])
+  const fetchOneMovie = async (id) => {
+    return await MovieAPI.getOneMovie(id).then((data) => setMovie(data));
+  };
 
-  
+  useEffect(() => {
+    fetchMovies().then((data) => setMovies(data));
+  }, [movies]);
 
   return (
     <>
-    <h1>Most Recent Movies</h1>
-    <CardGroup>
-
-    {movies.map((movie,idx) =>
-        <Card 
-        movie={movie}
-        />
-        )}
-        </CardGroup>
+      <h1>Most Recent Movies</h1>
+      <CardGroup>
+        {movies.map((movie, idx) => (
+          <ModalButton
+            onClick={() => {
+                setShowModal(true);
+                fetchOneMovie(movie.id);
+                console.log('help',movie.id)
+            }}
+            key={idx}
+          >
+            <Card movie={movie} />
+          </ModalButton>
+        ))}
+      </CardGroup>
+      <DetailModal movie={movie} showModal={showModal}/>
     </>
-    );
-
+  );
 };
 
 export default MovieList;
 
 const CardGroup = styled.div`
-    ${Flex({})};
-    flex-flow: row wrap;
-    margin: 80px 24px 27px 121px;
+  ${Flex({})};
+  flex-flow: row wrap;
+`;
+
+const ModalButton = styled.button`
+  border: none;
+  background-color: transparent;
 `;
