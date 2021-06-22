005e816a6f575fe8dc22b805f5ccb562bc3e6903
23carnies <46233990+23carnies@users.noreply.github.com>
2021-06-22 18:24:33 -0400

    Modal functionality and styling

--- src/components/DetailModal.jsx
+++ src/components/DetailModal.jsx
@@ -1,43 +1,65 @@
-import React from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { Flex } from "./utilities";
 import styled from "styled-components";
 import CloseX from "../images/close.svg";
-// import { MovieContext } from "../contexts/MovieContext";
-// import * as MovieAPI from "../services";
 
+const DetailModal = ({ showModal, setShowModal, movie }) => {
+  const modalRef = useRef();
 
+  const closeModal = (e) => {
+    if (modalRef.current === e.target) {
+      setShowModal(false);
+    }
+  };
 
-const DetailModal = ({ showModal, movie }) => {
+  const keyPress = useCallback(
+    (e) => {
+      if (e.key === "Escape" && showModal) {
+        setShowModal(false);
+      }
+    },
+    [setShowModal, showModal]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", keyPress);
+    return () => document.removeEventListener("keydown", keyPress);
+  }, [keyPress]);
 
   return (
-      <>{showModal ?  
-        <Background>
-    <Container>
-      <Box>
-        <h1>{movie.title}</h1>
-        <button style={{ border: "1px solid #141e35" }}>
-          <Close src={CloseX} alt="X" />
-        </button>
-      </Box>
-      <Box>
-        <Image
-          src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
-          alt={`${movie.title} cover`}
-        />
-        <InfoBox>
-          <p>
-            <b>Release date:</b>
-            {movie.release_date}
-          </p>
-          <p>{movie.overview}</p>
-          <p>
-            <b>{movie.vote_average} / 10</b> ({movie.vote_count} total votes)
-          </p>
-        </InfoBox>
-      </Box>
-    </Container>
-    </Background>
-    : null }
+    <>
+      {showModal ? (
+        <Background ref={modalRef} onClick={closeModal}>
+          <Container>
+            <TitleBox>
+              <Title>{movie.title}</Title>
+              <Button
+                aria-label="Close modal"
+                onClick={() => setShowModal(false)}
+              >
+                <Close src={CloseX} alt="Close" />
+              </Button>
+            </TitleBox>
+            <Box>
+              <Image
+                src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+                alt={`${movie.title} cover`}
+              />
+              <InfoBox>
+                <p>
+                  <b>Release date:</b>
+                  {movie.release_date}
+                </p>
+                <p>{movie.overview}</p>
+                <p>
+                  <b>{movie.vote_average} / 10</b> ({movie.vote_count} total
+                  votes)
+                </p>
+              </InfoBox>
+            </Box>
+          </Container>
+        </Background>
+      ) : null}
     </>
   );
 };
@@ -45,31 +67,55 @@ const DetailModal = ({ showModal, movie }) => {
 export default DetailModal;
 
 const Background = styled.section`
-    ${Flex};
-    background-color: rgba(0, 0, 0, 0.7);
-    position: fixed;
-    top: 0%;
-    left: 0%;
-    width: 100vw;
-    height: 100vh;
+  ${Flex};
+  background-color: rgba(0, 0, 0, 0.7);
+  position: fixed;
+  top: 0%;
+  left: 0%;
+  width: 100vw;
+  height: 100vh;
+  transform: translateY(0%);
+  transition: translateY(-100%);
 `;
 
 const Container = styled.section`
   ${Flex({ fd: "column" })};
   background-color: #fff;
   width: 583px;
-height: 474px;
+  height: 474px;
 `;
 
 const Box = styled.div`
+  ${Flex({ ai: "flex-start" })};
+  padding: 0 27px;
+`;
+
+const TitleBox = styled.div`
   ${Flex};
+  margin-top: -20px;
+  width: 100%;
+`;
+
+const Title = styled.h2`
+  font-weight: 700;
+  font-size: 18px;
 `;
 
 const Close = styled.img`
   width: 13px;
   height: 13px;
-  padding: 8.5px;
-  /* border: 1px solid #141e35; */
+  padding: 5px;
+`;
+
+const Button = styled.button`
+  ${Flex({ ai: "center", jc: "center" })};
+  background-color: #fff;
+  border: 0;
+  border: 1px solid #141e35;
+  padding: 0;
+  height: 22px;
+  width: 22px;
+  margin-left: 79%;
 `;
 
 const Image = styled.img`
@@ -77,4 +123,7 @@ const Image = styled.img`
   height: 389px;
 `;
 
-const InfoBox = styled.div``;
+const InfoBox = styled.div`
+  padding: 0 22px;
+  margin-top: 0;
+`;
