f7af01c0c9c606b843b20c2a526dcd0a9a29bf1d
23carnies <46233990+23carnies@users.noreply.github.com>
2021-06-24 13:49:57 -0400

    Add aria label

--- src/components/elements/Search.js
+++ src/components/elements/Search.js
@@ -1,29 +1,29 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import SearchIcon from '../../images/search.svg';
-import { Flex } from '../utilities';
+import SearchIcon from "../../images/search.svg";
+import { Flex } from "../utilities";
 
 const Search = ({ searchTerm, setSearchTerm }) => {
   return (
-      <Container>
-          <Icon src={SearchIcon} alt="Search icon" aria-hidden="true" />
-          <Input
-            type="text"
-            placeholder="Search for a movie"
-            // aria-controls="search"
-            onChange={(e) => {
-                setSearchTerm(e.target.value);
-            }}
-          ></Input>
-
-      </Container>
+    <Container>
+      <Icon src={SearchIcon} alt="Search icon" aria-hidden="true" />
+      <Input
+        title="search"
+        type="text"
+        placeholder="Search for a movie"
+        aria-label="search"
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      ></Input>
+    </Container>
   );
 };
 
 export default Search;
 
 const Container = styled.div`
-  ${Flex({ai:'center'})}
+  ${Flex({ ai: "center" })}
   width:180px;
   height: 36px;
   border: 1px solid #ccc;
@@ -32,14 +32,13 @@ const Container = styled.div`
   :focus-within Icon {
     display: none;
   }
-
 `;
 
 const Icon = styled.img`
   padding: 0 1% 0 2%;
   width: 20px;
   height: 20px;
-  z-index:20;
+  z-index: 20;
   /* position: absolute;
   top: 0;
   left: 0; */
@@ -48,5 +47,5 @@ const Icon = styled.img`
 const Input = styled.input`
   border: none;
   padding: 0.5rem;
-  outline:none;
+  outline: none;
 `;
